5991e65c898a44f0fbbebcf50f3a249de0dbbab5
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-12-18 00:00:44 +0530

    UseEffect Hook,Live Swiggy api called and render live data

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
 import "./Body.css";
 import resData from "../../utils/data";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resData);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/search/v3?lat=20.27060&lng=85.83340&str=popular%20dish&trackingId=0ad2e8eb-1af7-0c4f-450a-b45766d836ed&submitAction=ENTER&queryUniqueId=76cda06c-152e-50a5-3137-21d619a47b9c"
+    );
+    const json = await data.json();
+
+    // console.log(json);
+
+    // loading dynamics data from swigy api
+
+    // setListOfRestaurants(json?.data?.card[1]?.data?.data?.cards);
+  };
 
   return (
     <>
